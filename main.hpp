cc1e873662cc4ca6ac359562fb1587004fc09266
RobertoGol <gol89264@gmail.com>
2026-06-12 16:50:17 +0300

    Update Errors V2.7

--- main.hpp
+++ main.hpp
@@ -5,6 +5,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <d3d11.h>
+#include <d3dcompiler.h> // <--- УБЕДИСЬ, ЧТО ЭТА СТРОКА ТУТ ЕСТЬ
 #include <vector>
 #include <string>
 #include "Types.hpp"
