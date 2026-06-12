a328489a2d6835835dfc393d1f6335473f69ca60
RobertoGol <gol89264@gmail.com>
2026-06-12 17:22:47 +0300

    Update Errors V2.9

--- main.hpp
+++ main.hpp
@@ -6,24 +6,26 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <d3d11.h>
-#include <d3dcompiler.h>
+#include <d3dcompiler.h> // Полностью закрывает ошибку D3DCompile
 #include <vector>
 #include <string>
 #include <algorithm>
 
-#include "Types.hpp"
-#include "Tactics.hpp"
+// ИСПРАВЛЕНО: Подключение строго по путям твоих вложенных папок
+#include "Data/Types.hpp"
+#include "Data/Physics/Tactics.hpp"
+#include "Data/Map.cpp" // Для доступа к константам разметки, если требуется
+#include "Data/bodies/Enemies.hpp"
+#include "Data/Physics/Mechanics.hpp"
+#include "Data/Player/Player.hpp"
+#include "Data/HUD/HUD.hpp"
+#include "Data/HUD/UIEffects.hpp"
 
-// ИСПРАВЛЕНО: Все инклуды модулей подключаются СТРОГО здесь
-#include "Enemies.hpp"
-#include "Mechanics.hpp"
-#include "Player.hpp"
-
-// Forward declarations системных функций
+// Forward declarations системных функций для оконного цикла
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 void UpdateHelldiversCamera(float deltaTime);
 
-// ОБЪЯВЛЕНИЕ ГЛОБАЛЬНЫХ ПЕРЕМЕННЫХ (extern для линкера)
+// ОБЪЯВЛЕНИЕ ГЛОБАЛЬНЫХ ПЕРЕМЕННЫХ (extern связывает модули через линкер)
 extern UnitMode playerMode;
 extern Vector3D playerPos;
 extern float playerHealth;
@@ -49,7 +51,7 @@ extern float playerErosionLevel;
 extern bool isAiming;
 extern float baseZoom;
 
-// DIRECTX 11 СИСТЕМНЫЕ УКАЗАТЕЛИ
+// DIRECTX 11 СИСТЕМНЫЕ УКАЗАТЕЛИ ДВИЖКА
 extern ID3D11Device*           g_pd3dDevice;
 extern ID3D11DeviceContext*    g_pd3dDeviceContext;
 extern IDXGISwapChain*         g_pSwapChain;
@@ -59,12 +61,12 @@ extern ID3D11PixelShader*      g_pPixelShader;
 extern ID3D11InputLayout*      g_pInputLayout;
 extern ID3D11Buffer*           g_pVertexBuffer;
 
-// СЕТКА КАРТЫ СЕКТОРОВ
+// СЕТКА КАРТЫ СЕКТОРОВ УБЕЖИЩА 17
 extern int currentSectorMap[MAP_WIDTH][MAP_HEIGHT];
 extern int wallDurability[MAP_WIDTH][MAP_HEIGHT];
 extern float etherErosionMap[MAP_WIDTH][MAP_HEIGHT];
 
-// Глобальные хелперы рендера
+// Глобальные аппаратные хелперы рендера геометрии
 void PushCircle(std::vector<Vertex>& buffer, float cx, float cy, float r, float width, float height, Vertex col, int segments = 12);
 ScreenPoint PixelsToNDC(float x, float y, float width, float height);
 void CreateRenderTarget();
